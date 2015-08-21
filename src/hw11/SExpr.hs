d3f352cac1bdb3ca97d05bb7418f73da91a9e576
David Peterson <dhpeterson@gmail.com>
2015-08-21 11:44:58 +1000

    Completed hw11

--- src/hw11/SExpr.hs
+++ src/hw11/SExpr.hs
@@ -6,26 +6,40 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char (isAlpha, isAlphaNum, isSpace)
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
+--
+-- either parse a character, or return [] in the failure case
+--
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure [] -- note, order is important!
 
+--
+-- parses a character, and then attempts to parse another (which may fail)
+--
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = liftA2 (:) p (zeroOrMore p)
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
+--
+-- parse whitespace
+--
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore (satisfy isSpace)
 
+--
+-- Parses an identifier, which is defined to be an alphabetic char followed by
+-- zero or more alphanumeric characters.
+--
 ident :: Parser String
-ident = undefined
+ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore $ satisfy isAlphaNum)
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -44,3 +58,28 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+
+--
+-- Parse s-expressions
+--
+-- An s-expression is defined here as either an atom or a list of
+-- s-expressions. An s-expression can start with any number of leading
+-- or trailing spaces, which need to be thrown away.
+--
+parseSExpr :: Parser SExpr
+parseSExpr =
+  spaces *> (atom <|> sexp) <* spaces
+  where
+    atom = liftA A parseAtom
+    sexp = liftA Comb $ char '(' *> oneOrMore parseSExpr <* char ')'
+
+--
+-- Parse atoms
+--
+-- An atom is defined as an identifier (lit) or an integer (int) value
+parseAtom :: Parser Atom
+parseAtom = int <|> lit
+  where
+    int = liftA N posInt
+    lit = liftA I ident
